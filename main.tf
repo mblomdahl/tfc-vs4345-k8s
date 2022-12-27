332a78140603ca8afccbd11098c89be2f989bd2e
Mats Blomdahl <mats.blomdahl@gmail.com>
2022-12-27 14:59:44 +0100

    Try resolving "aws-auth" ConfigMap issue
    Using suggestion from: https://github.com/terraform-aws-modules/terraform-aws-eks/issues/2009#issuecomment-1100628225

--- main.tf
+++ main.tf
@@ -1,11 +1,15 @@
-# Kubernetes provider
-# https://learn.hashicorp.com/terraform/kubernetes/provision-eks-cluster#optional-configure-terraform-kubernetes-provider
-# To learn how to schedule deployments and services using the provider, go here: https://learn.hashicorp.com/terraform/kubernetes/deploy-nginx-kubernetes
-# The Kubernetes provider is included in this file so the EKS module can complete successfully. Otherwise, it throws an error when creating `kubernetes_config_map.aws_auth`.
-# You should **not** schedule deployments and services in this workspace. This keeps workspaces modular (one for provision EKS, another for scheduling Kubernetes resources) as per best practices.
+data "aws_eks_cluster" "default" {
+  name = local.cluster_name
+}
+
+data "aws_eks_cluster_auth" "default" {
+  name = local.cluster_name
+}
+
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.eks_auth.token
 }
 
 provider "aws" {
