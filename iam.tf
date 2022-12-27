afb492c83c906cac55caaca920b8f8c41b267eef
Mats Blomdahl <mats.blomdahl@gmail.com>
2022-12-27 23:09:55 +0100

    Add IAM policy to EKS workers for managing AWS ALB
    From https://github.com/k-mitevski/terraform-k8s/blob/master/03_terraform_alb_ingress/main.tf

---
+++ iam.tf
@@ -0,0 +1,13 @@
+resource "aws_iam_policy" "worker_policy" {
+  name        = "eks-worker-policy"
+  description = "EKS worker policy for the ALB Ingress"
+
+  policy = file("iam-policies/eks-worker-policy.json")
+}
+
+resource "aws_iam_role_policy_attachment" "additional" {
+  for_each = module.eks.eks_managed_node_groups
+
+  policy_arn = aws_iam_policy.worker_policy.arn
+  role       = each.value.iam_role_name
+}
