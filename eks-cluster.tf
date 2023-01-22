ce79c6a69b7cd29a9297f7214732dc7b8942af02
Mats Blomdahl <mats.blomdahl@gmail.com>
2023-01-22 17:37:30 +0100

    Bump AWS module version and add better node names

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -27,7 +27,7 @@ module "eks" {
 
   eks_managed_node_groups = {
     one = {
-      name = "node-group-1"
+      name = "${var.resource_prefix}-node-group-1"
 
       instance_types = [var.node_group_instance_type]
 
@@ -37,7 +37,7 @@ module "eks" {
     }
 
     two = {
-      name = "node-group-2"
+      name = "${var.resource_prefix}-node-group-2"
 
       instance_types = [var.node_group_instance_type]
 
